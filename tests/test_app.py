b448664c69e10478e084a9ce971c27d6b803363e
PedroGhost007 <prcpinto.04@gmail.com>
2025-05-22 19:52:32 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
